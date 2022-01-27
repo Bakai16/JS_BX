83ebb083af5d48186155264aded556287928cd78
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-01-27 20:19:15 +0600

    Added task1

--- Task - 1/task4.js
+++ Task - 1/task4.js
@@ -5,7 +5,7 @@ let numberTrinkets = +prompt('Канча оюнчук каалайсыз?');
 let gramTrinkets = 112;
 
 let totalWeight = numberSouvenirs * gramSouvenirs + numberTrinkets * gramTrinkets;
-console.log(`Жалпы салмагы- ${totalWeight} `);
+console.log(`Жалпы салмагы- ${totalWeight} гр `);
 
 
 
