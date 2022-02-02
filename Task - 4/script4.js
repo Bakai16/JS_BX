30323042c0d54407e2829a85610c78ce35c8d5f7
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-02 21:07:53 +0600

    Lesson 4 updated

--- Task - 4/script4.js
+++ Task - 4/script4.js
@@ -1,15 +1,29 @@
-let user = "Bakai";
+// let user = "Bakai";
 
-switch (user){
-    case "John":
-        console.log("You are not user");
-        break;
-    case "Hanna":
-        console.log("You are moderator");
-        break;
-    case "Bakai":
-        console.log("You are admin");
-        break;   
-    default:
-    console.log("I don't know you!");
+// switch (user){
+//     case "John":
+//         console.log("You are not user");
+//         break;
+//     case "Hanna":
+//         console.log("You are moderator");
+//         break;
+//     case "Bakai":
+//         console.log("You are admin");
+//         break;   
+//     default:
+//     console.log("I don't know you!"); 
+// }
+
+
+//Тернарного оператора   ? :   единственный оператор
+
+// let user = "John";
+// console.log(user === "John" ? "You are John" : "You are not John");
+
+
+
+function greet(name){
+    console.log(`Hello ${name}`);
 }
+greet("Bakai");
+//expected output : Hello Bakai
