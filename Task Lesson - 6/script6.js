dae53bf37e59b05414bd167201bfafb21468330a
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-12 23:24:33 +0600

    lesson 6

--- Task Lesson - 6/script6.js
+++ Task Lesson - 6/script6.js
@@ -1,11 +1,11 @@
-let arr = [1, 2, 3, 1, 2, 3, 1, 6, 2, 1, 3, 4];
+let arr = [1, 2, 3, 1, 2, 3, 1, 2, 6, 3, 4];
 function dedude(arr){
     let result = [];
     arr.forEach(element => {
         if (result.length === 0) {
             result.push(element);
         } else if (!result.includes(element)){
-            result.pust(element);
+            result.push(element);
         }
     });
     return result;
