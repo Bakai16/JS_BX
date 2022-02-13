8b91feb73d86ae6f6f950334048aae0efbf34614
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-13 14:22:37 +0600

    lesson 5 четные числа

---
+++ Task Lesson - 6  не дубликат/script6.js
@@ -0,0 +1,26 @@
+let arr = [1, 2, 3, 1, 2, 3, 1, 2, 6, 3, 4];
+// function dedude(arr){
+//     let result = [];
+//     arr.forEach(element => {
+//         if (result.length === 0) {
+//             result.push(element);
+//         } else if (!result.includes(element)){
+//             result.push(element);
+//         }
+//     });
+//     return result;
+// }
+// console.log(dedude(arr));
+//expected output: [1, 2, 3, 6, 4]
+
+
+let unicArr = new Set(arr);  // преобразование массива в множество
+unicArr = Array.from(unicArr);
+console.log(unicArr);
+//expected output: [1, 2, 3, 6, 4]
+
+
+
+let unicsArr = [...new Set(arr)];
+console.log(unicsArr);
+//expected output: [1, 2, 3, 6, 4]
