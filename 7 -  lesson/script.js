6cbde20f59114ff8f28fb0377b7f1c92353013dc
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-13 22:14:46 +0600

    lesson 7

--- 7 -  lesson/script.js
+++ 7 -  lesson/script.js
@@ -1,9 +1,19 @@
 const title = document.createElement("h1");
 title.innerText = "Таблица умножения";
 
-for (let a = 1; a <= 9 ; a++){
-    for (let b = 1; b <= 9; b++){
+for (let a = 1; a < 9 ; a++){
+    for (let b = 1; b < 9; b++){
        const div = document.createElement("div");
-        div.innerText =a + ' *' + b + ' = ' + (a*b) + '<br>';
+        div.innerText = a + ' *' + b + ' = ' + (a*b) + '<br>';
     }
 }
+
+
+
+
+const wrapperChild = document.querySelectorAll(".wrapper > div");
+
+const box = document.querySelector(".box");
+
+
+console.log(box.closest(".grand-wrapper"));
