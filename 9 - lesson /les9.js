7129b628cb4c4b3918466b49b43d053b9268e865
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-21 00:14:20 +0600

    Lesson 9

--- 9 - lesson /les9.js
+++ 9 - lesson /les9.js
@@ -1,6 +1,17 @@
-const btn = document.querySelector('.btn');
-const title = document.querySelector('h1');
+// const btn = document.querySelector('.btn');
+// const title = document.querySelector('h1');
 
-btn.addEventListener("click", function(){
-    title.classList.add('active');
+// btn.addEventListener("click", function(){
+//     title.classList.add('active');
+// });
+
+
+const divs = document.querySelectorAll(".box");
+
+divs.forEach((item) =>{
+    item.addEventListener("click", () =>{
+        item.classList.toggle("active"); 
+    });
 });
+
+console.log(divs);
