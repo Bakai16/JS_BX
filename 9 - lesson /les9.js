38490d8211b08c75bf2f05f3ec1a5ec8627dc7c5
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-21 02:50:56 +0600

    lesson 9

--- 9 - lesson /les9.js
+++ 9 - lesson /les9.js
@@ -6,12 +6,54 @@
 // });
 
 
-const divs = document.querySelectorAll(".box");
+////11
+// const divs = document.querySelectorAll(".box");
 
-divs.forEach((item) =>{
-    item.addEventListener("click", () =>{
-        item.classList.toggle("active"); 
+// divs.forEach((item) =>{
+//     item.addEventListener("click", () =>{
+//         item.classList.toggle("active"); 
+//     });
+// });
+
+// console.log(divs);
+
+// window.addEventListener('load', function(){
+//     const div = document.querySelector(".box");
+    
+//     div.addEventListener("click", addClass);
+    
+//     function addClass(e){
+//         e.target.parentNode.classList.add("active");
+//     }
+// });
+
+
+window.addEventListener("load", function (){
+  const box = document.querySelector(".box");
+
+
+  box.addEventListener("mouseleave", function(){ ///mousedown   mouseover   
+    box.style.background = "red";
+
+ });
+
+//   const link = document.querySelector(".link");
+  
+//     link.addEventListener("click", function(e){
+//         e.preventDefault(); 
+//     });
+//     document.addEventListener("keydown", function(e){
+//         console.log('keed');
+//     });
+
+    const input = document.querySelector(".input");
+    const btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", function (){
+        const userInput = input.value;
+
+
+        input.value = "";
+        alert(userInput);
     });
 });
-
-console.log(divs);
