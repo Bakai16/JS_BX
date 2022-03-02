ae1953ba3a262e26b20b7a152a8c383d19b6c3ba
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-03-03 01:35:30 +0600

    lesson 10

--- 10 - lesson/le10.js
+++ 10 - lesson/le10.js
@@ -1,15 +1,25 @@
 const container = document.querySelector(".container");
 const ul = document.querySelector("ul");
-const li = document.querySelector("li"); 
+const lis = document.querySelectorAll("li"); 
 
-li.addEventListener("click", () => {
-    console.log("li");
+// li.addEventListener("click", () => {
+//     console.log("li");
+// });
+
+// ul.addEventListener("click", () => {
+//     console.log("ul");
+// }, true);
+
+// container.addEventListener("click", () => {
+//     console.log("container");
+// }, true);
+
+lis.forEach((li) => {
+    li.addEventListener("click", () => {
+        console.log("li");
+    });
 });
 
-ul.addEventListener("click", () => {
-    console.log("ul");
-});
-
-container.addEventListener("click", () => {
-    console.log("container");
+container.addEventListener(("click"), (e)=> {
+    console.log(e.target.textContent);
 });
