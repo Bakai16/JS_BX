fa3bdfaa2adb5b6999e4596d72c467158eba73f0
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-03-03 21:34:50 +0600

    Todo-List

--- 10 todo-list/todo.js
+++ 10 todo-list/todo.js
@@ -12,7 +12,23 @@ function loadEventListeners(){
     task.addEventListener("click", removeTask);
 
     clrBtn.addEventListener("click", clearTasks);
+
+    filter.addEventListener("keyup", filterTasks);
 } 
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll(".collection-item").forEach(task =>  {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = "block";
+        }else{
+            task.style.display = "none";
+        }
+        });
+}
+
 
 function clearTasks() {
     // taskList.innerHTML = "";
