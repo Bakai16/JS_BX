f6fbeb31d3158222ab9e71dcbb99969fc4b6a79d
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-03-12 04:07:06 +0600

    TodoLIst LocalStorage

--- 11 - local_storage/sclocal.js
+++ 11 - local_storage/sclocal.js
@@ -8,14 +8,14 @@ const num = 25;
 
 //localStorage.clear();    // clear all items
 
-const user = {
-    name: "Bakai",
-    age: "25",
-};
+// const user = {
+//     name: "Bakai",
+//     age: "25",
+// };
 
-localStorage.setItem('user', JSON.stringify(user)); 
+// localStorage.setItem('user', JSON.stringify(user)); 
 
-const userFromLS = JSON.parse(localStorage.getItem("user"));
-console.log(userFromLS);
+// const userFromLS = JSON.parse(localStorage.getItem("user"));
+// console.log(userFromLS);
 
-console.log(userFromLS.name);
+// console.log(userFromLS.name);
