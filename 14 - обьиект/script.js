daeb19d2cc787d51842eb08c3467070d8566c0af
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-03-15 11:17:35 +0600

    Конструктор

--- 14 - обьиект/script.js
+++ 14 - обьиект/script.js
@@ -18,11 +18,39 @@
 
 // Bakai.sayName();
 
-function Person(name, age){     /// конструктор болщой буквы
+
+//222konstruktor
+
+function Person(name, birthday){     /// конструктор болщой буквы
     this.name = name;
-    this.age = age; 
+    // this.age = age; 
+    this.birthday = new Date(birthday);
+
+    // console.log(this);
+    // this.sayName = function (){
+    //     console.log(`Hello my name is: ${this.name}`);
+    // };
+
+    // this.calculateAge = function () {};
 }
 
-const Joth = new Person('Joth', 25);
+Person.prototype.calculateAge = function (){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return ageDate.getUTCFullYear() - 1970;
+};
 
-console.log(Joth);
+const Bakai = new Person("Bakai", '01-01-2003');
+const Sanjar = new Person("Sancho", '01-01-2001');
+console.log(Bakai.calculateAge());
+console.log(Sanjar.calculateAge());
+Sanjar.sayName();
+Bakai.sayName();
+
+
+//333 конструктор и прототип
+// const myStr = "djsferjijef";
+// const myStr2 = new String("djsferjijef");
+
+// console.log(myStr);
+// console.log(myStr2);
