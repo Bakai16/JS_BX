1e8d1de27eb2e87564e7cb76b9525abbaab05d8d
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-03-15 22:56:35 +0600

    NumberGuess

--- 13 - number.guesser_no_js/guess.js
+++ 13 - number.guesser_no_js/guess.js
@@ -1,6 +1,6 @@
 let min = 1;
     max = 10;
-    winningNum = 2;
+    winningNum = getRandomNum(min, max);
     guessesLeft = 3;
 
 
@@ -24,13 +24,52 @@ guessBtn.addEventListener("click", function (){
 
     //check if won
     if(guess === winningNum){
-        guessInput.disabsled = true;
-        guessInput.style.border = "1px solid green";
-        setMessage(`Поздравляю! Вы угадали число ${winningNum}!` , 'green');
+        // guessInput.disabsled = true;
+        // guessInput.style.border = "1px solid green";
+        // setMessage(`Поздравляю! Вы угадали число ${winningNum}!` , 'green');
+        gameOver(true, `Поздравляю! вы угадал число  ${winningNum}`);
+    }else {
+        guessesLeft -=1;
+         
+        if(guessesLeft === 0){
+            gameOver(false, `Вы проиграли! Правилный ответ ${winningNum}`);
+        }
+        else {
+            guessInput.style.border = "1px solid red";
+            guessInput.value = "";
+
+            setMessage(`Неверно! У вас осталотсь ${guessesLeft} попыток`, "red");
+        }
     }
 });
+
+game.addEventListener("mousedown", function (e){
+    if (e.target.className === "play again"){
+        window.location.reload();
+    }
+});
+
+function gameOver(won, msg){
+    let color;
+    won === true ? (color = "green") : (color = "red");
+
+    guessInput.disabled = true;
+    guessInput.style.border = `1px solid ${color}`;
+    message.style.color = color;
+    setMessage(msg);
+
+    //play again 
+    guessBtn.textContent = "Начать занова";
+    guessBtn.className += "play-again";
+
+}
+
 
 function setMessage(msg, color){
     message.textContent = msg;
     message.style.color = color;
+}
+
+function getRandomNum (min, max){
+    return Math.floor(Math.random() * (max - min + 1) + max);
 }
