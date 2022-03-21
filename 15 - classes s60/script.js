d0f3d2abeab1077fb68b7ea5ce3874ca81e6a06d
Akylbekov <user@Users-MacBook-Pro.local>
2022-03-21 23:08:32 +0600

    booklist

--- 15 - classes s60/script.js
+++ 15 - classes s60/script.js
@@ -42,6 +42,7 @@ class Restaurant{
     constructor(name, cusintype){
         this.name = name;
         this.cusintype = cusintype;
+        this.served = 0;
     }
 
     showInfo(){
@@ -62,15 +63,29 @@ class Restaurant{
         return `${this.name} is closed`;
     }
     }
+    
 
     
 }
+   Serve(){
+        return(this.served += 1);
+
+    }
+    setServed(){
+       return (this.served += num);
+    }
+    showtotalServer(){
+        return (`Show total served: ${this.served}`);
+    }
 }
 
 const bayKazan = new Restaurant ( "BayKazan", "Kyrgyz");
 const Vasya = new Restaurant ( "Vasya", "Russian");
 
 console.log(bayKazan.isOpen());
+console.log(bayKazan.Serve());
+console.log(bayKazan.setServed(29));
+console.log(bayKazan.showtotalServer());
 // console.log(Vasya.showInfo());
 
 
---
+++ 16 - booklist/script.js
@@ -0,0 +1,28 @@
+//Book constructor
+function Book (){}
+
+//Ui contructor 
+function UI(){}
+
+//UI prototype methods
+UI.prototype.addBookList = function (book){
+    //find the table
+    //Create tr element
+    //Insert Cols
+    //Append to table
+};
+
+
+//clear fields
+UI.prototype.clearFields = function(){};
+
+
+//Event listeners
+document.getElementByID("book-form").addEventListener("submit", (e) => {
+    //Get form values
+    //Instance of book
+    //Instance of UI
+    //Add book to list
+    //clear fields
+});
+
