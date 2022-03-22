9cf8397b15d04a8d547e741c9ff2cce2739fcc1a
Akylbekov <user@Users-MacBook-Pro.local>
2022-03-22 12:17:10 +0600

    javascript booklist show Alert

--- 16 - booklist/script.js
+++ 16 - booklist/script.js
@@ -1,28 +1,77 @@
-//Book constructor
-function Book (){}
+/* Book Constructor */
+function Book(title, author, isbn) {
+   this.title = title;
+   this.author = author;
+   this.isbn = isbn;
+ }
 
-//Ui contructor 
-function UI(){}
+/* UI Constructor  */
+ function UI() {}
 
-//UI prototype methods
-UI.prototype.addBookList = function (book){
-    //find the table
-    //Create tr element
-    //Insert Cols
-    //Append to table
-};
+/* UI Prototype Methods */
+ UI.prototype.addBookToList = function (book) {
+   const list = document.getElementById("book-list");
+   const row = document.createElement("tr");
 
+   row.innerHTML = `
+     <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
 
-//clear fields
-UI.prototype.clearFields = function(){};
+  list.appendChild(row);
+ };
+
+// Show Alert
+ UI.prototype.showAlert = function (msg, className) {
+   const div = document.createElement("div");
+   div.className = `alert ${className}`;
+
+   div.appendChild(document.createTextNode(msg));
+
+  const container = document.querySelector(".container");
+   const form = document.querySelector("#book-form");
+
+  container.insertBefore(div, form);
+
+   setTimeout(function () {
+     document.querySelector(".alert").remove();
+   }, 3000);
+ };
+
+ //Delete Book
+ UI.prototype.deleteBook = function (target) {
+   if (target.className === "delete") {
+     target.parentElement.parentElement.remove();
+   }
+ };
+// Clear fields
+  UI.prototype.clearFields = function () {
+   document.getElementById("title").value = "";
+   document.getElementById("author").value = "";
+   document.getElementById("isbn").value = "";
+ };
 
 
 //Event listeners
-document.getElementByID("book-form").addEventListener("submit", (e) => {
-    //Get form values
-    //Instance of book
-    //Instance of UI
-    //Add book to list
-    //clear fieldsп
+document.getElementById("book-form").addEventListener("submit", (e) => {
+    const title = document.getElementById("title").value,
+          author = document.getElementById("author").value,
+          isbn = document.getElementById("isbn").value;
+
+          const book = new Book(title, author, isbn);
+          const ui = new UI();
+
+          //validate
+          if (title === "" || author === "" || isbn === "") {
+            ui.showAlert("Please fill in all fields!", "error");
+          } else {
+            ui.addBookToList(book);
+            ui.showAlert("Book added!", "success");
+            ui.clearFields();
+          }
+
+          e.preventDefault(); 
 });
 
