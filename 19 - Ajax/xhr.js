3bb2803144954b0a5566958b899c930e59eb29c4
Akylbekov <user@Users-MacBook-Pro.local>
2022-03-24 11:12:33 +0600

    xhr Ajax

--- 19 - Ajax/xhr.js
+++ 19 - Ajax/xhr.js
@@ -7,15 +7,27 @@
 document.getElementById('get-data').addEventListener("click", loadData);
 
 function loadData(){
-    const xhr = new HMLHttpReguest(); 
+    const xhr = new XMLHttpRequest(); 
 
     xhr.open("Get", "db.txt", true);
 
     xhr.onload = function ()              /////в процессе 
     {
         if(this.status === 200){
-            console.log(this.response);
+            console.log(this.response);   //response    ответ   жоок    отклик
+            document.querySelector(".output").innerHTML =`
+            <p>
+                ${this.response}
+             </p>`; 
+        }else if(this.status === 404) {
+            alert("Can not connect to DB!");
         }
     };
-    
+
+    xhr.onprogress = function(){
+        console.log(xhr.readyState);
+    };
+
+
+    xhr.send();
 }
