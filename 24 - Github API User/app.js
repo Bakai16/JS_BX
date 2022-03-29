b9dbca58782f5faf307390498dba34fe319e88a5
Akylbekov <user@Users-MacBook-Pro.local>
2022-03-29 16:02:27 +0600

    showalert

--- 24 - Github API User/app.js
+++ 24 - Github API User/app.js
@@ -3,16 +3,19 @@ const ui = new UI();
 
 const searchInput = document.getElementById("search-user");
 
-searchInput.addEventListener('keyup', (e) =>{
-    const userText = e.target.value;
+searchInput.addEventListener("keyup", (e) => {
+  const userText = e.target.value;
 
-    if(userText !== ""){
-        github.getUser(userText).then((data)=>{
-            if(data.profile.message === "Not found"){
-
-            }else{
-                ui.showProfile(data.profile);
-            }
-        });
-    }
+  if (userText !== "") {
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        ui.showAlert("User Not Found! Sorry!", "alert alert-danger");
+      } else {
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    ui.clearProfile();
+  }
 });
--- 24 - Github API User/git.js
+++ 24 - Github API User/git.js
@@ -4,19 +4,26 @@ class Github {
     constructor() {
         this.client_id = "dd17b25e7ebb42dc40c5 ";
         this.client_secret = "cf397bc9a91d1a4bd75ed86d91b9ae18819b4230";
+        this.repos_count = 10;
+        this.repos_sort = "created: asc";
     }
 
 
     async getUser(user) {
-        const profileResponce = await fetch
-        (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        const profileResponse = await fetch(
+          `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
         );
-
-        const profile = await profileResponce.json();
-
+    
+        const repoResponse = await fetch(
+          `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+    
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+    
         return {
-            profile: profile,
+          profile,
+          repos,
         };
+      }
     }
-
-}
