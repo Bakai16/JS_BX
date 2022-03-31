a3a9ab50fcf24db1ee0031ff65465938d8da71b8
Akylbekov <user@Users-MacBook-Pro.local>
2022-03-31 14:02:51 +0600

    Reference Error  SyntaxError

---
+++ 27 - errors/app.js
@@ -0,0 +1,29 @@
+// try {
+//     greet();
+// }catch (error){
+//     console.log(error.name);
+// }finally{
+//     console.log("This is finally content");
+// }
+
+// console.log("heejfje");
+
+// function greet(){
+//     console.log("HElelofef");
+// }
+
+///>>>/????Practice
+
+const user = { name: "Asan" };
+
+try {
+    if(!user.email){
+        // throw new SyntaxError("user does not have an email");
+        throw new ReferenceError("user does not have an email");
+    }
+} catch(e){
+    // console.log(e.message);
+    console.log(e);
+}
+
+//expect output ---> eferenceError: user does not have an email
